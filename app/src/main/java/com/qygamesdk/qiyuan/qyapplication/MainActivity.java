ef09f4764e14ce5a67570d0b1112c7bc119018e4
fyc <925355740@qq.com>
2018-11-22 18:20:15 +0800

    修改payH5接口，将payUrl拆分为多参数进行传参

--- app/src/main/java/com/qygamesdk/qiyuan/qyapplication/MainActivity.java
+++ app/src/main/java/com/qygamesdk/qiyuan/qyapplication/MainActivity.java
@@ -109,7 +109,13 @@ public class MainActivity extends FragmentActivity {
     }
 
     private void payH5(String payUrl) {
-        QYGameSDK.getInstance().payH5(MainActivity.this, "http://www.373yx.com", payUrl, null);
+//        QYGameSDK.getInstance().payH5(MainActivity.this, "http://www.373yx.com", payUrl, null);
+        Long cliBuyerId = 19000L ;
+        String cliSellerId = "2018111415564890400010102c2";
+        String cpOrderNo = System.currentTimeMillis() + "";
+        String cpOrderTitle = "%E9%A6%96%E5%85%851";
+        float cpPrice = 0.01f;
+        QYGameSDK.getInstance().payH5(MainActivity.this, cliBuyerId, cliSellerId, cpOrderNo, cpOrderTitle, cpPrice);
     }
 
     @Override
